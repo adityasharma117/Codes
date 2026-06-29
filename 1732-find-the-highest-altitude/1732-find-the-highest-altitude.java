48437cab9543dbe6fa4499b78fe5c32fcd6d29ec
kar-17 <132463005+kar-17@users.noreply.github.com>
2026-06-29 18:51:34 +0530

    Time: 0 ms (100%), Space: 43.1 MB (57.19%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -2,15 +2,12 @@
 class Solution {
     public int largestAltitude(int[] gain) {
         
-        for(int i = 1; i<gain.length; i++){
-            gain[i] = gain[i]+gain[i-1];
-            
-        }
-      Arrays.sort(gain);
-      int max = gain[gain.length-1];
-      if(max>=0)
-      return max;
-      else return 0;
-
-        }
+    int alt = 0;
+    int maxal = 0;
+    for(int i=0;i<gain.length;i++)
+    {
+        alt=alt+gain[i];
+    maxal = Math.max(maxal,alt);
     }
+    return maxal;
+    }}
