49aa305fa5035c029c8462f9f16a6b97e272c591
kar-17 <132463005+kar-17@users.noreply.github.com>
2026-07-01 16:27:06 +0530

    Time: 3 ms (57.26%), Space: 42.9 MB (95.67%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,10 +1,5 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        char b='b';
-        char a='a';
-        char l='l';
-        char o='o';
-        char n='n';
        int ba=0;
        int aa=0;
        int la=0;
@@ -36,9 +31,7 @@ class Solution {
         min=Math.min(min,la);
         min=Math.min(min,oa);
         min=Math.min(min,na);
-        if(ba!=0 && aa!=0 && la!=0 && oa!=0 && na!=0){
         return min;
-        }
-        else return 0;
+        
     }
     }
