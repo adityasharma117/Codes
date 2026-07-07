d31eefd4deb3c1a898d1b7f55782bc2bfd13e864
kar-17 <132463005+kar-17@users.noreply.github.com>
2026-07-07 22:11:46 +0530

    Time: 1 ms (99.85%), Space: 42.4 MB (83.11%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -2,23 +2,21 @@ class Solution {
     public long sumAndMultiply(int n) {
         if(n==0){return 0;}
         long k = 0;
-        long kk = 0;
+
         int sum = 0;
         int r = -1;
+        int i = 1;
         while(n>0){
              if(n%10!=0){            
                  r=n%10;
                  sum+=r;
-                k = (k*10)+r;
+                k+= (r*i);
+                i*=10;
              }      
 
             n=n/10;
         }
-        while(k>0){
-            long rr = k%10;
-            kk = (kk*10)+rr;
-            k=k/10;
-        }
-        return kk*sum;
+       
+        return (long)k *sum;
     }
 }
