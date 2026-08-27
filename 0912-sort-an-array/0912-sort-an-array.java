e03c363acd38ab96a210580556ea90321b3509ed
kar-17 <132463005+kar-17@users.noreply.github.com>
2026-08-27 15:22:58 +0530

    Time: 1484 ms (5.09%), Space: 77.7 MB (90.36%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,51 +1,41 @@
 class Solution {
-public static void mergesort(int[] arr, int low, int high){
-     
-    if(low<high){
-        int mid = low+(high-low) / 2;
-        mergesort(arr,low,mid);
-        mergesort(arr,mid+1,high);
-        merge(arr,low,mid,high);
-    }
-} 
-    public static void merge(int[] a, int low, int mid, int high)
-{
-    int i = low;
-    int j = mid + 1;
-    int k = low;
-
-    int[] c = new int[a.length];
-
-    while (i <= mid && j <= high) {
-        if (a[i] < a[j]) {
-            c[k] = a[i];
-            i++;
+    public void merge(int[] arr, int low, int mid, int high){
+        int i = low;
+        int j = mid + 1;
+        int k = low;
+        int[] temp = new int [arr.length];
+        while(i<=mid && j<=high){
+            if(arr[i]<arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }
+            else{ temp[k] = arr[j];j++;}
+            k++;
         }
-        else {
-            c[k] = a[j];
-            j++;
+        for(;i<=mid;i++){
+            temp[k]=arr[i];
+            k++;
         }
-        k++;
+        for(;j<=high;j++){
+            temp[k]=arr[j];
+            k++;
+        }
+        for(int l = low; l<=high; l++){
+            arr[l] = temp[l];
+        }
+    }
+    public void mergesort(int[] arr, int low, int high){
+        if(low<high){
+            int mid = low + (high-low) / 2;
+            mergesort(arr, low, mid);
+            mergesort(arr, mid+1, high);
+            merge(arr, low, mid, high);
+        }
     }
 
-    for (; i <= mid; i++) {
-        c[k] = a[i];
-        k++;
-    }
 
-    for (; j <= high; j++) {
-        c[k] = a[j];
-        k++;
-    }
-
-    for(int l = low; l<=high;l++)
-    {
-        a[l]=c[l];
-    }
-}
     public int[] sortArray(int[] nums) {
-         mergesort(nums,0,nums.length - 1);
-    return nums;
+        mergesort(nums, 0, nums.length - 1);
+        return nums;
+    }
 }
-}
- 
