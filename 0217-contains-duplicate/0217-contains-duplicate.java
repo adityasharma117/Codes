57ca575f8e44e9d50de0c542f0ffb2c6501cab29
kar-17 <132463005+kar-17@users.noreply.github.com>
2026-08-30 22:56:12 +0530

    Time: 19 ms (48.81%), Space: 111.7 MB (5.57%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        boolean k = true;
+        boolean k = false;
         HashMap <Integer,Integer> mpp = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
             if(!mpp.containsKey(nums[i])){
                 mpp.put(nums[i],i);
-                k = false;
             }
             else{ k = true; break;}
 
